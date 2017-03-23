02dc12378306c78fd6f627636aebfa5fe773a480
HeringAndras <hering.bandi@gmail.com>
2017-03-23 22:22:04 +0100

    lista generátor

--- src/homework-03.R
+++ src/homework-03.R
@@ -3,3 +3,16 @@
 # 2016/17. II. félév
 # Hering András
 # 2017.03.23
+
+# lista <- list(round((runif(n = 5,min = 1,max = 2)*10), digits = 0))
+
+
+y <- NULL
+y <- as.list(y)
+for ( i in 1:5) {
+  y[[i]]<-runif( n = round(runif(1,1,2)*10,0), min = 1, max = 2)
+}
+
+y
+
+
